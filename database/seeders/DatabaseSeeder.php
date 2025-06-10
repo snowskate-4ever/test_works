37ed59c8ef03ee2363539ffa8bb518090a52b89b
madmd <madmd@yandex.ru>
2025-06-10 14:33:15 +0300

    Products,categories factory and seeds with properties

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Property;
+use App\Models\Type;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\CategoryFactory;
+use Database\Factories\PropertyFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +21,43 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+        Category::factory(15)->create();
+        Product::factory(1000)->create();
+        $products = Product::all();
+        $types = Type::where('slug', 'products')->get();
+        foreach ($products as $product) {
+            foreach ($types as $type) {
+                if (!Property::where([
+                    'type_id' => $type->id,
+                    'product_id'=> $product->id,
+                    'slug' => 'products'
+                ])->exists()) {
+                    switch ($type->name) {
+                        case 'Вес':
+                            $unit = 'кг';
+                            break;
+                        case 'Ширина' || 'Высота' || 'Длина':
+                            $unit = 'см';
+                            break;
+
+                        default:
+                            $unit = '';
+                            break;
+                    }
+                    $properties = [
+                        'product_id' => $product->id,
+                        'type_id' => $type->id,
+                        'unit' => $unit,
+                        'slug' => 'products',
+                        'value' => random_int(1, 100),
+                    ];
+                    Property::create($properties);
+                }
+            }
+        }
     }
 }
